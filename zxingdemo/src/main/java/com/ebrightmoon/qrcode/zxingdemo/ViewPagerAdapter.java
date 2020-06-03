bac3cad7b0c7b6c26224f05d2c756f2ea225e058
ebrightmoon <2315813288@qq.com>
2020-06-03 15:51:21 +0800

    代码结构

---
+++ zxingdemo/src/main/java/com/ebrightmoon/qrcode/zxingdemo/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.ebrightmoon.qrcode.zxingdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Time: 2020/6/2
+ * Author:wyy
+ * Description:
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments;
+
+    public ViewPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
